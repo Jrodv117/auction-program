a5617a6c65c44303022b36d5da6a7fb5e61d18d0
jrodr117 <jrodr113@msudenver.edu>
2022-09-20 12:11:21 -0600

    implemented function that find highest bidder and bid. Implemented main as point of execution

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-from gavel import logo
 import os
+from gavel import logo
 
 print(logo)
 
 users_dictionary = {}
 
-def kill_switch(switch):
-    if switch == 'yes':
-        os.system('clear')
+
+def kill_switch(flag):
+    """kills code when user chooses to exit"""
+    if flag == "yes":
+        os.system("clear")
     else:
         print(users_dictionary)
         find_highest_bid()
         exit()
 
-def find_highest_bid():
-    for key in users_dictionary:
-        if users_dictionary[key]:
-            exit()
-        
 
-while True:
-    name = input("Bidder's Name: ")
-    bid = float(input("What's your bid: "))
-    users_dictionary[name] = bid
-    switch = input("Are there other bidder (yes/no)? ")
-    kill_switch(switch)
-    
+def find_highest_bid():
+    """finds the highest bid and bidder"""
+    highest_bidder = max(users_dictionary, key=users_dictionary.get)
+    highest_bid = int((max(users_dictionary.values())))
+    print(f"The winning bidder is: {highest_bidder} with a bid of: ${highest_bid}")
+
+
+def main():
+    """main function that is the point of execution"""
+    while True:
+        name = input("Bidder's Name: ")
+        bid = float(input("What's your bid: "))
+        users_dictionary[name] = bid
+        switch = input("Are there other bidder (yes/no)? ")
+        kill_switch(switch)
+
+
+if __name__ == "__main__":
+    main()
